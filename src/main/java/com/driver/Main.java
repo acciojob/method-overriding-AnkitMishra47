d4f78dc7c33a807e115780a91c5255aa7ece2434
Ankit Mishra <ankitm17.2001@gmail.com>
2022-10-15 17:29:13 +0530

    files added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        A obj = new B();
+        System.out.println(obj.meth());
+    }
+
+  static class A{
+      String meth(){
+          return "Invoking method from class A";
+      }
+  }
+  static class B extends A{
+      String meth(){
+          return "Invoking method from class B";
+      }
+  }
 }
